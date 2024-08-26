b3f22550b6695eb444c00438f36e5ad2ff0b575a
v012345 <v138468@gmail.com>
2024-08-26 18:30:24 +0800

    HUD

--- 2d-dodge/main.gd
+++ 2d-dodge/main.gd
@@ -12,11 +12,15 @@ func _ready() -> void:
 func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
 
 func new_game():
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
+	get_tree().call_group("mobs", "queue_free")
 
 
 func _on_start_timer_timeout() -> void:
@@ -26,6 +30,7 @@ func _on_start_timer_timeout() -> void:
 
 func _on_score_timer_timeout() -> void:
 	score += 1
+	$HUD.update_score(score)
 
 
 func _on_mob_timer_timeout() -> void:
