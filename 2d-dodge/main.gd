6115e02e21876ac8a62a148da6f1c44ca08ceb74
v012345 <v138468@gmail.com>
2024-08-27 15:46:06 +0800

    finish

--- 2d-dodge/main.gd
+++ 2d-dodge/main.gd
@@ -13,6 +13,8 @@ func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
 	$HUD.show_game_over()
+	$Music.stop()
+	$DeathSound.play()
 
 func new_game():
 	score = 0
@@ -21,6 +23,7 @@ func new_game():
 	$HUD.update_score(score)
 	$HUD.show_message("Get Ready")
 	get_tree().call_group("mobs", "queue_free")
+	$Music.play()
 
 
 func _on_start_timer_timeout() -> void:
