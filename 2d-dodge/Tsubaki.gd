c515671e2561ebc3b6c238b13a9f521a54162d29
v012345 <v138468@gmail.com>
2024-08-30 15:37:02 +0800

    add socket

--- 2d-dodge/Tsubaki.gd
+++ 2d-dodge/Tsubaki.gd
@@ -1,13 +1,87 @@
 extends CanvasLayer
-
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	print("jjjjjj")
-	print(_G.gd)
+	#var output = []
+	#print(OS.execute("D:\\NightOwlToolsV2\\NightOwlToolsV2\\NightOwlToolsV2.exe",["D:\\NightOwlToolsV2\\NightOwlToolsV2\\main.lua"],output,true))
+	##print(OS.get_executable_path())
+	##pid = OS.create_process("D:\\NightOwlToolsV2\\NightOwlToolsV2\\NightOwlToolsV2.exe", ["D:\\NightOwlToolsV2\\NightOwlToolsV2\\main.lua"])
+	#print(pid)
+ #
+#
+	#for x in output:
+		#print(x)
+	##print("jjjjjj")
+	##print(_G.gd)
+	#var result = client.connect_to_host("127.0.0.1", 12345)
+	#if result != OK:
+		#print("Failed to connect to server!")
+	#else:
+		#print("Connected to server!")
+	##var data = "jjjjjjjj"
+	##client.set_no_delay(true)
+	#print(client.STATUS_CONNECTED)
+	#if pid and !OS.is_process_running(pid):
+		#print("Process has terminated")
+		#pid = null
+	#else:
+		#print("gpgpg")
+	#print(client.poll())
+	#if client.get_available_bytes() > 0:
+		#var data = client.get_utf8_string(client.get_available_bytes())
+		#print("Received from server: ", data)
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	#if pid and !OS.is_process_running(pid):
+		##print("Process has terminated")
+		##pid = null
+		#pass
+	#else:
+		#pass
+		#print("gpgpg")
 	pass
+
+
+func _on_button_pressed() -> void:
+	Socket.put_utf8_string("_on_button_pressed\n")
+	OS.kill(Socket.pid)
+	#print(OS.is_process_running(pid))
+	#if pid and !OS.is_process_running(pid):
+		#print("Process has terminated")
+		#pid = null
+	#var output = []
+	##OS.execute("powershell.exe", ["-Command", "D:\\NightOwlToolsV2\\NightOwlToolsV2\\NightOwlToolsV2.exe"], output)
+	##print(OS.execute("D:/NightOwlToolsV2/NightOwlToolsV2/NightOwlToolsV2.exe",[],output))
+	##var output = []
+	##OS.execute("D:\\c.exe", ["jldj","ji"], output)
+	##for x in output:
+		##print(x)
+	#
+	##client.put_utf8_string("jjjjjjjjj")
+	##client.put_data("Hello world".to_utf8_buffer())
+	#print(client.poll())
+	#var _status = client.get_status()
+	#print(_status)
+	#if client.get_available_bytes() > 0:
+		#var data = client.get_utf8_string(client.get_available_bytes())
+		#print("Received from server: ", data)
+	#if _status != client.STATUS_CONNECTED:
+		#print("Error: Stream is not currently connected.")
+		##return false
+	#var data = "jiiehfsfiefjdkjfd\n"
+	#client.put_utf8_string(data)
+	#client.flush()
+	#print(client.poll())
+		#return false
+	#return true
+	#if client.get_available_bytes() > 0:
+		#data = client.get_utf8_string(client.get_available_bytes())
+		#print("Received from server: ", data)
+	pass # Replace with function body.
+
+
+func _on_button_literally_pressed() -> void:
+	Socket.put_utf8_string("_on_button_literally_pressed\n")
+	pass # Replace with function body.
---
+++ 2d-dodge/socket.gd
@@ -0,0 +1,56 @@
+extends Node
+
+var client := StreamPeerTCP.new()
+var is_connected := false
+var pid = null
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	# 尝试连接服务器
+	client.connect_to_host("127.0.0.1", 12345)
+	client.poll()
+	#await get_tree().create_timer(1).timeout
+	if client.get_status() == StreamPeerTCP.STATUS_CONNECTED:
+		print("Connected to server.")
+		is_connected = true
+		# 启动心跳计时器
+		start_heartbeat()
+		return
+	else:
+		client.disconnect_from_host()
+		#OS.kill(pid)
+		print("Failed to connect to server.")
+	pass # Replace with function body.
+
+# 心跳发送逻辑
+func _on_heartbeat_timeout():
+	if is_connected:
+		client.put_utf8_string("ping\n")
+		
+# 开始心跳机制
+func start_heartbeat():
+	var heartbeat_timer = Timer.new()
+	heartbeat_timer.wait_time = 1
+	heartbeat_timer.one_shot = false  # 循环触发
+	heartbeat_timer.autostart = true  # 自动启动
+	heartbeat_timer.connect("timeout",Callable(self, "_on_heartbeat_timeout"))
+	add_child(heartbeat_timer)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if is_connected and client.get_available_bytes() > 0:
+		print(client.get_available_bytes())
+		var message = client.get_utf8_string(client.get_available_bytes()).strip_edges()
+		if message:
+			print(message)
+	pass
+	
+func put_utf8_string(s):
+	if is_connected:
+		client.put_utf8_string(s)
+
+# 处理连接断开或超时
+func disconnect_from_host():
+	if is_connected:
+		client.disconnect_from_host()
+		is_connected = false
+		print("Disconnected from server.")
